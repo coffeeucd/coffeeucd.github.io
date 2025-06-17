e8a085eb658facf538b692efcfdd4b22a04b2020
Kavi <rkavinaya@gmail.com>
2025-06-17 13:07:47 -0700

    initial

---
+++ coffee-ucd/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Events from './pages/Events'
+//import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        {/* You can add Navbar here if you want */}
+        <main className="flex-grow px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/events" element={<Events />} />
+          </Routes>
+        </main>
+        {/* You can add Footer here */}
+      </div>
+    </Router>
+  )
+}
+
+export default App
