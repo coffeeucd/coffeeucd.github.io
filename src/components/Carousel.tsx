709b6179b1ffb63f339fd0f89ea59324c26e1d89
acarva203 <137564086+acarva203@users.noreply.github.com>
2025-09-14 01:10:10 -0700

    add 2 photos

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,9 +5,11 @@ import img3 from "../assets/img3.jpg";
 import img4 from "../assets/img4.jpg";
 import img5 from "../assets/img5.jpg";
 import img6 from "../assets/img6.jpg";
+import img7 from "../assets/img7.jpeg";
+import img8 from "../assets/img8.jpeg";
 import { useState, useEffect } from "react";
 
-const originalImages = [img1, img2, img3, img4, img5, img6];
+const originalImages = [img1, img2, img3, img4, img5, img6, img7, img8];
 
 /** ---- SIZING CONTROLS ----
  * MAX_WIDTH: overall width cap (e.g., "1100px", "72rem", "100%")
